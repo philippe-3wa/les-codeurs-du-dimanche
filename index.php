62322bab155a12cd08cb4136a5d8be4d1fd63fc2
JohnMoller <johnny.moller.67@gmail.com>
2016-05-18 10:44:40 +0200

    index.php modifications

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 session_start();
 $error = '';
 $page = 'home';
-<<<<<<< HEAD
+
 $access = array('home', 'login', 'register', 'admin', 'logout', 'article');
-=======
+
 $access = array('home', 'atricle', 'login', 'register', 'admin', 'profile', 'logout');
->>>>>>> ab5245d0ce7086d25ba0e069ebfe0299b0513f37
+
 if (isset($_GET['page']))
 {
 	if (in_array($_GET['page'], $access))
