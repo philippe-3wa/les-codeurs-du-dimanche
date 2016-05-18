d03e4ad305db0ac4d897ba3ead09b6987b4295f9
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-18 09:52:16 +0200

    modif index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 session_start();
 $error = '';
 $page = 'home';
-$access = array('home', 'login', 'register', 'admin', 'logout');
+$access = array('home', 'atricle', 'login', 'register', 'admin', 'logout');
 if (isset($_GET['page']))
 {
 	if (in_array($_GET['page'], $access))
 		$page = $_GET['page'];
 }
-$access_traitement = array('login', 'register', 'admin', 'logout');
+$access_traitement = array('login', 'article', 'register', 'admin', 'logout');
 if (in_array($page, $access_traitement))
 	require('apps/traitement_'.$page.'.php');
 require('apps/skel.php');
