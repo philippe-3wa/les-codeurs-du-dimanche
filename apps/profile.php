d88fde46da17a318dc786a80ea1bab8707c31b90
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-18 16:28:47 +0200

    profile

--- apps/profile.php
+++ apps/profile.php
@@ -1,4 +1,14 @@
 <?php
-if (isset($_SESSION['login']))
-	require('views/profile.phtml');
+if ( isset($_SESSION['role']))
+{
+	if ($_SESSION['role'] == 1)
+		require('views/profile_admin.phtml');
+	else
+		require('views/profile.phtml');
+}
+else
+{
+	header('Location: index.php?page=login');
+	exit;
+}
 ?>
