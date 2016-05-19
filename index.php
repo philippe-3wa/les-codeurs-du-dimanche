72081864e657accdec7bc299df7f5af2e7653c45
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-19 10:45:18 +0200

    connexion mysql

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+
+require('config.php');
+$link = mysqli_connect($db_host, $db_login, $db_pass, $db_name);
+
+if (!$link) 
+{
+    require('views/mysql_error.php');
+}
+
 $error = '';
 $message = '';
 $page = 'home';
@@ -15,4 +24,6 @@ $access_traitement = array('login', 'register', 'admin', 'logout');
 if (in_array($page, $access_traitement))
 	require('apps/traitement_'.$page.'.php');
 require('apps/skel.php');
+
+mysqli_close($link);
 ?>
