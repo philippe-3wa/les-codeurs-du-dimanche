a6c1bb2fffa002c90fa07f97bb8705b5a8cb03fa
JohnMoller <johnny.moller.67@gmail.com>
2016-05-19 14:32:44 +0200

    modif' articles.php

--- apps/article.php
+++ apps/article.php
@@ -1,6 +1,6 @@
 <?php
 if (!empty($error))
-	require('views/error.phtml');
+	require('apps/error.php');
 
 
 if (isset($_GET['id']))
@@ -8,16 +8,18 @@ if (isset($_GET['id']))
 
 	$id = intval($_GET['id']);
 
-	$query = 'SELECT id, titre, contenu, date, auteur FROM articles WHERE id="'.$id.'"';
+	$query = 'SELECT users.id AS users_id, articles.id AS articles_id, 
+	articles.titre, articles.contenu, articles.date AS articles_date, 
+	articles.auteur FROM articles INNER JOIN users ON articles.auteur = users.id';
 	$res = mysqli_query($link, $query);
 
 	while ($ligne = mysqli_fetch_assoc($res))
 	{
 		
-		$id = $ligne['id'];
+		$id = $ligne['articles_id'];
 		$titre = $ligne['titre'];
 		$contenu = $ligne['contenu'];
-		$date = $ligne['date'];
+		$date = $ligne['articles_date'];
 		$auteur = $ligne['auteur'];
 	}
 
