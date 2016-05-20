d95b30cc86bf09a52edc1dc1f87040de016a6ccc
JohnMoller <johnny.moller.67@gmail.com>
2016-05-20 15:30:03 +0200

    Modif'

--- apps/commentaires_action.php
+++ apps/commentaires_action.php
@@ -1,3 +1,31 @@
 <?php
-	
+
+if (isset($_SESSION['role'], $_GET['action'], $_GET['id']))
+{
+	$id_commentaire_edit = intval($_GET['id']);
+
+	if ($_GET['action'] == "edit")
+	{
+		$query = 'SELECT * FROM commentaires WHERE id="'.$id_commentaire_edit.'"'; 
+		$res = mysqli_query($link, $query);
+		
+		while ($ligne = mysqli_fetch_assoc($res))
+			{
+				if (($_SESSION['role'] == 1) || ($_SESSION['id'] == $ligne['auteur']))
+				{
+					$commentaire_id=$ligne['id'];
+					$commentaire_contenu=$ligne['contenu'];
+					$commentaire_auteur=$ligne['auteur'];
+					$commentaire_id_article=$ligne['id_article'];
+					require('views/commentaires_form_in_edit.phtml');
+				}
+				else
+				{
+					header('Location: index.php?page=login');
+					exit;
+				}
+			}
+	}	
+
+}
 ?>
--- apps/traitement_commentaires_action.php
+++ apps/traitement_commentaires_action.php
@@ -1,19 +1,26 @@
 <?php
-if (isset($_SESSION['login']))
-{
 	if (isset($_POST['commentaire']))
 	{
 		$commentaire = $_POST['commentaire'];
 		$id_article = $_POST['id_article'];
 		$auteur = $_SESSION['id'];
-		$query = "INSERT INTO commentaires (contenu, auteur, id_article) VALUES ('$commentaire', '$auteur', '$id_article')";
+
+		if (isset($_POST['id_commentaire']))
+		{
+			$id_new = $_POST['id_commentaire'];
+			$query = "UPDATE commentaires SET contenu='".$commentaire."' WHERE id='".$id_new."'";
+		}
+		else
+		{
+			$query = "INSERT INTO commentaires (contenu, auteur, id_article) VALUES ('$commentaire', '$auteur', '$id_article')";
+		}
+		
+		
 		$res = mysqli_query($link, $query);
+
 		header('Location: index.php?page=article&id='.$id_article.'');
 		exit;
-
-
 	}
-header('Location: index.php?page=login');
-exit;
-}
+
+
 ?>
