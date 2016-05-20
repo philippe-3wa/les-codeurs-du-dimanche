2ea50b64dfbe9b06e6a5487ca7ba028fdc7cdb22
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-20 16:53:24 +0200

    message

--- apps/traitement_commentaires_action.php
+++ apps/traitement_commentaires_action.php
@@ -1,23 +1,35 @@
 <?php
 	if (isset($_POST['commentaire']))
 	{
+
 		$commentaire = $_POST['commentaire'];
 		$id_article = $_POST['id_article'];
 		$auteur = $_SESSION['id'];
 
-		if (isset($_POST['id_commentaire']))
-		{
-			$id_new = $_POST['id_commentaire'];
-			$query = "UPDATE commentaires SET contenu='".$commentaire."' WHERE id='".$id_new."'";
-		}
-		else
-		{
-			$query = "INSERT INTO commentaires (contenu, auteur, id_article) VALUES ('$commentaire', '$auteur', '$id_article')";
-		}
-		
-		
-		$res = mysqli_query($link, $query);
+		if (strlen($commentaire) < 3)
+			$error = 'commentaire trop court !';
 
+		if (empty($error))
+		{
+
+			if (isset($_POST['id_commentaire']))
+			{
+				$id_new = $_POST['id_commentaire'];
+				$query = "UPDATE commentaires SET contenu='".$commentaire."' WHERE id='".$id_new."'";
+			}
+			else
+			{
+				$query = "INSERT INTO commentaires (contenu, auteur, id_article) VALUES ('$commentaire', '$auteur', '$id_article')";
+			}
+			
+			
+			$res = mysqli_query($link, $query);
+
+			header('Location: index.php?page=article&id='.$id_article.'');
+			exit;
+
+		}
+		else 
 		header('Location: index.php?page=article&id='.$id_article.'');
 		exit;
 	}
