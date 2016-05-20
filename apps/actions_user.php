823f26e2a8b8704ad60f0c5ca1cc932fe36731b1
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-20 13:27:38 +0200

    commentaires modifs

--- apps/actions_user.php
+++ apps/actions_user.php
@@ -1,4 +1,9 @@
 <?php
-if (isset($_SESSION['role']) && $_SESSION['role'] == 0)
-	require('views/actions_user.phtml');
+if ($_SESSION['role'] == 1)
+	require('views/actions_commentaires_admin.phtml');
+else if ($_SESSION['role'] == 0)
+{
+	if ($commentaires_auteur == $_SESSION['id'])
+		require('views/actions_commentaires_user.phtml');
+}
 ?>
