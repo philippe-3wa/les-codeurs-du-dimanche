0dfa8f4ee1d14c71a0e8f32a845311a3e1301a70
philippe-3wa <dossantos.philippe@outlook.com>
2016-05-20 13:16:26 +0200

    edit article

--- apps/admin.php
+++ apps/admin.php
@@ -10,7 +10,36 @@ if (isset($_SESSION['role']) && $_SESSION['role'] == 1)
 		exit;
 	}
 	else
-	require('views/admin.phtml');
+	{
+		if (isset($_GET['action'], $_GET['id']) && $_GET['action'] == 'editer')
+		{
+			$id = intval($_GET['id']);
+
+			$query = 'SELECT users.id AS users_id, articles.id AS articles_id, 
+			articles.titre, articles.contenu, articles.date AS articles_date, 
+			articles.auteur, users.login FROM articles INNER JOIN users ON articles.auteur = users.id
+			WHERE articles.id = '.$id.'';
+			$res = mysqli_query($link, $query);
+
+
+			while ($ligne = mysqli_fetch_assoc($res))
+			{
+		
+				$id_article = $ligne['articles_id'];
+				$titre = $ligne['titre'];
+				$contenu = $ligne['contenu'];
+				$date = $ligne['articles_date'];
+				$id_auteur = $ligne['auteur'];
+				$auteur = $ligne['login'];
+			}
+		require('views/article_edit.phtml');
+		}
+		else {
+			require('views/admin.phtml');
+		}
+	}
+
+	
 }
 else
 {
--- apps/traitement_admin.php
+++ apps/traitement_admin.php
@@ -9,17 +9,25 @@ if (isset($_SESSION['login']))
 
 		if (strlen($title) < 3)
 			$error = 'Titre trop court ! (< 3 caractères)';
-		else if (strlen($title) > 32)
-			$error = 'Titre trop long ! (> 32 caractères)';/** Pascal : Dans votre db le titre fait 255 caractères maxi et pas 32 ! **/
+		else if (strlen($title) > 255)
+			$error = 'Titre trop long ! (> 255 caractères)';
 		if (strlen($content) < 16)
 			$error = 'Contenu trop court ! (< 16 caractères)';
-		else if (strlen($content) > 1024)
-			$error = 'Contenu trop long ! (> 1024 caractères)';/** Pascal : Le contenu dans votre db fait 1023 maxi, attention ! **/
+		else if (strlen($content) > 1023)
+			$error = 'Contenu trop long ! (> 1023 caractères)';
 	
 		if (empty($error))
 		{
-			$query = "INSERT INTO articles (titre, contenu, auteur) VALUES ('$title', '$content', '$auteur')";/** Pascal : On concatene plutot que d'utiliser des trucs obscures de php :o **/
-			$res = mysqli_query($link, $query);
+			if ($_POST['action'] == "editer")
+			{	
+				$id_edit = $_POST['id'];
+				$query = "UPDATE articles SET titre='".$title."', contenu='".$content."' WHERE id='".$id_edit."'";
+			}
+			else
+			{
+				$query = "INSERT INTO articles (titre, contenu, auteur) VALUES ('".$title."', '".$content."', '".$auteur."')";/** Pascal : On concatene plutot que d'utiliser des trucs obscures de php :o **/
+			}
+			mysqli_query($link, $query);
 
 			header('Location: index.php');
 			exit;
